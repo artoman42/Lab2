72941aab9f7f651c789928d159c67fd8fdfb6f9e
artoman42 <70898630+artoman42@users.noreply.github.com>
2023-04-24 01:23:04 +0300

    Before architecture changed

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,18 +1,26 @@
 ﻿using LibraryBLL;
 using LibraryDAL;
 using LibraryUIL;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Options;
 namespace Lab2
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
-            IServiceCollection serviceDescriptors = new ServiceCollection();
-            serviceDescriptors.AddServices();
-            IServiceProvider serviceProvider = serviceDescriptors.BuildServiceProvider();
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent
+            .Parent.FullName + "/LibraryDAL/appsettings")
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            var serviceProvider = new ServiceCollection()
+            .Configure<MyConfiguration>(options => configuration.GetSection("MyConfiguration")
+            .Bind(options))
+            .AddServices()
+            .BuildServiceProvider();
             serviceProvider.GetService<IRunner>().Run();
         }
     }
